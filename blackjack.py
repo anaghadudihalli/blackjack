a4e4ac094ded40dc56a61f20c45f7b69e39e2dbd
Anagha Dudihalli <anaghadudihalli@gmail.com>
2019-04-16 08:24:43 +0530

    Add the flow. Need to fix the bug

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from time import sleep
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
@@ -27,7 +28,7 @@ class Card:
         self.rank = rank
 
     def __str__(self):
-        print(f'{self.rank} of {self.suit}')
+        return f'{self.rank} of {self.suit}'
 
 
 class Deck:
@@ -44,10 +45,13 @@ class Deck:
         shuffle(self.cards_deck)
 
     def __str__(self):
-        print(self.cards_deck)
+        cards_str = ''
+        for card in self.cards_deck:
+            cards_str += '\n' + card.__str__()
+        return cards_str
 
     def draw_card(self):
-        self.cards_deck.pop()
+        return self.cards_deck.pop()
 
 
 class Hand:
@@ -60,15 +64,12 @@ class Hand:
         self.cards.append(card)
         self.value += ranks_to_values[card.rank]
 
+        if card.rank == 'Ace':
+            self.aces += 1
+
         if self.value > 21 and self.aces:
             self.value -= 10
             self.aces -= 1
-
-    def __str__(self):
-        for card in self.cards:
-            print(f'{card.rank} of {card.suit}')
-
-        print(f"Your hand's value is {self.value}")
 
 
 def place_bet():
@@ -89,25 +90,153 @@ def place_bet():
                 break
 
 
-def hit(deck, hand):
-    card = deck.draw_card()
+def hit(deck_, hand):
+    card = deck_.draw_card()
     hand.add_card(card)
 
 
-def hit_or_stand(deck, hand):
+def hit_or_stand(deck_, hand):
+    global playing
     playing = True
 
     while playing:
         player_input = input('Do you want to hit or stand? h/s: ').lower()
+        print("Inside hit or stand")
 
         if player_input == 'h':
-            hit(deck, hand)
+            print("In h")
+            hit(deck_, hand)
         elif player_input == 's':
+            print("In s")
             playing = False
         else:
             print("That's not a valid input. Try again")
+            continue
+        break
 
 
+def show_some_cards(player_, dealer_):
+    print("Dealer:")
+    for index, card in enumerate(dealer_.cards):
+        if index == 1:
+            print('Face Down')
+        else:
+            print(card)
+
+    print("You:")
+    for card in player_.cards:
+        print(card)
+    print("Your hand value: " + str(player_.value))
 
 
+def show_all_cards(player_, dealer_):
+    print("Dealer:")
+    for card in dealer_.cards:
+        print(card)
+    print("Dealer's hand value: " + str(dealer_.value))
 
+    print("You:")
+    for card in player_.cards:
+        print(card)
+    print("Your hand value: " + str(player_.value))
+
+
+def player_busts():
+    global chips, bet
+    print("Player busts!")
+    chips -= bet
+
+
+def player_wins():
+    global chips, bet
+    print("Player wins!")
+    chips += bet
+
+
+def dealer_busts():
+    global chips, bet
+    print("Dealer busts!")
+    chips += bet
+
+
+def dealer_wins():
+    global chips, bet
+    print("Dealer wins!")
+    chips -= bet
+
+
+def tie():
+    print("Dealer and Player tie!")
+
+
+def replay():
+    global chips
+    global bet
+
+    replay_choice = input("Do you want to play again? y/n?").lower()
+
+    if replay_choice == 'y' and chips > 0:
+        while True:
+            chips_choice = input("Do you want to continue with same number of chips or renew them? s/r? ").lower()
+            if chips_choice == 's':
+                bet = 0
+                return True
+            elif chips_choice == 'r':
+                chips = 0
+                bet = 0
+                return True
+            else:
+                print("Invalid input. Please try again.")
+                continue
+
+
+if __name__ == '__main__':
+    print('\nWelcome to BlackJack! Get ready to play!! ')
+    playing = True
+    while True:
+        print('\nShuffling the deck ... Gimme a min')
+        deck = Deck()
+        deck.shuffle()
+        sleep(2)
+        print(f'Alrighty! You have {chips} chips.')
+        place_bet()
+        print('Dealing cards ..')
+        player = Hand()
+        dealer = Hand()
+        for _ in range(0, 2):
+            hit(deck, player)
+            hit(deck, dealer)
+        show_some_cards(player, dealer)
+
+        while playing:
+            hit_or_stand(deck, player)
+            show_some_cards(player, dealer)
+
+            if player.value > 21:
+                player_busts()
+                break
+
+        if player.value <= 21:
+
+            while dealer.value < 17:
+                hit(deck, dealer)
+
+            show_all_cards(player, dealer)
+
+            if dealer.value > 21:
+                dealer_busts()
+
+            elif dealer.value > player.value:
+                dealer_wins()
+
+            elif dealer.value < player.value:
+                player_wins()
+
+            else:
+                tie()
+
+        print(f"\nPlayer's winnings stand at {chips}")
+
+        if not replay():
+            print("Thanks for playing!")
+            break
